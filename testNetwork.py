77e2b30e6d44e4ab4c93115d15a2d63b7ee855fc
RuiFilipe2802 <ruifilipe1229@gmail.com>
2021-10-29 12:00:57 +0100

    add

--- testNetwork.py
+++ testNetwork.py
@@ -1,11 +1,17 @@
 from icmplib import ping
 from icmplib import traceroute
+from time import ctime
+from datetime import datetime
 import sys
+import ntplib
 
 ip = sys.argv[1]
+##  IP
 host = ping(ip, count=10, interval=0.2)
 print(host)
 print("---------------")
+
+##  TRACEROUTE
 hops = traceroute(ip)
 print('Distance/TTL    Address    Average round-trip time')
 last_distance = 0
@@ -17,3 +23,10 @@ for hop in hops:
     print(f'{hop.distance}    {hop.address}    {hop.avg_rtt} ms')
     last_distance = hop.distance
 
+print("---------------")
+##  LATENCY
+c = ntplib.NTPClient()
+response = c.request('europe.pool.ntp.org', version=3)
+print(ctime(response.tx_time))
+now = datetime. now(). time()
+print("now =", now)
