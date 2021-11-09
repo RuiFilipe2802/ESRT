f252299f0883f20b0f6bfa1325d548e9811fe3da
julioliveira11 <a89393@alunos.uminho.pt>
2021-11-09 17:26:05 +0000

    Pacotes atualizados

--- peers.py
+++ peers.py
@@ -1,5 +1,6 @@
 import socket
 import base64
+import time
 
 host = '127.0.0.1'  # Standard loopback interface address (localhost)
 port = 9998       # Port to listen on (non-privileged ports are > 1023)
@@ -10,8 +11,58 @@ def connect(ip):
     array = ip.split(".")
     for a in range(len(array)):
         con.append(int(array[a]))
+    con[2]=time.gmtime(0)
     return con
+
+def disconnect(ip):
+    dis = bytearray(1)
+    dis[0] = 0b1
+    array = ip.split(".")
+
+    for a in range(len(array)):
+        dis.append(int(array[a]))
+    dis[2]=time.gmtime(0)
+    return dis
+
+def error(ip):
+    err = bytearray(1)
+    err[0] = 0b10
+    array = ip.split(".")
+
+    for a in range(len(array)):
+        err.append(int(array[a]))
+    err[2]=time.gmtime(0)
+    return err
     
+def send_neighbors(ip,ip1,ip2,porta):
+    send=bytearray(1)
+    send[0]=0b11
+    array = ip.split(".")
+    array2 = ip1.split(".")
+    array3 = ip2.split(".") 
+
+    for a in range(len(array)):
+        send.append(int(array[a]))
+    for b in range(len(array2)):
+        send.append(int(array2[b]))
+    for c in range(len(array3)):
+        send.append(int(array3[c]))
+
+    send[4]=time.gmtime(0)
+    send[5]=porta
+
+    return send
+    
+    
+def clock_adj(ip):
+    c_adj = bytearray(1)
+    c_adj[0] = 0b100
+    array = ip.split(".")
+
+    for a in range(len(array)):
+        c_adj.append(int(array[a]))
+    c_adj[2]=time.gmtime(0)
+    return c_adj
 
 if __name__ == "__main__":
 
