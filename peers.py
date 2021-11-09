0397d48d8841f338757f9d7b2776127c235ffe28
JoaoFerreira5 <joao.ferreira.585@gmail.com>
2021-11-09 16:53:38 +0000

    atualizaçao

--- peers.py
+++ peers.py
@@ -1,8 +1,8 @@
 import socket
 import base64
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 9998       # Port to listen on (non-privileged ports are > 1023)
+host = '127.0.0.1'  # Standard loopback interface address (localhost)
+port = 9998       # Port to listen on (non-privileged ports are > 1023)
 
 def connect(ip):
     con = bytearray(1)
@@ -14,9 +14,21 @@ def connect(ip):
     
 
 if __name__ == "__main__":
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        send = connect(socket.gethostbyname(socket.gethostname()))
-        s.connect((HOST, PORT))
-        s.sendall(send)
-        data = s.recv(1024)
-    print('Received', repr(data))
+
+    ClientSocket = socket.socket()
+
+    print('Waiting for connection')
+    try:
+        ClientSocket.connect((host, port))
+    except socket.error as e:
+        print(str(e))
+
+    Response = ClientSocket.recv(1024)
+    
+    while True:
+        Input = input('Say Something: ')
+        ClientSocket.send(str.encode(Input))
+        Response = ClientSocket.recv(1024)
+        print(Response.decode('utf-8'))
+
+    ClientSocket.close()
--- server.py
+++ server.py
@@ -2,9 +2,19 @@ import socket
 import os
 from _thread import *
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 9998       # Port to listen on (non-privileged ports are > 1023)
+host = '127.0.0.1'  # Standard loopback interface address (localhost)
+port = 9998       # Port to listen on (non-privileged ports are > 1023)
 
+
+def thread_client(connection):
+    connection.send(str.encode('Welcome to the Server'))
+    while True:
+        data = connection.recv(2048)
+        reply = 'Server Says: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(reply))
+    connection.close()
 
 if __name__ == "__main__":
     '''with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -29,4 +39,13 @@ if __name__ == "__main__":
     except socket.error as e:
         print(str(e))
     print('Waiting for Connection')
+    ServerSocket.listen(5)
+
+    while True:
+        Client, address = ServerSocket.accept()
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
+        start_new_thread(thread_client,(Client, ))
+        ThreadCount += 1
+        print('Thread Number: ' + str(ThreadCount))
+    ServerSocket.close()
     
