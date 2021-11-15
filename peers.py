4e00a70cb3d0502696bd33136d2e7b5db1602c57
julioliveira11 <a89393@alunos.uminho.pt>
2021-11-15 15:10:15 +0000

    getTime

--- peers.py
+++ peers.py
@@ -12,49 +12,51 @@ def getTime():
     c = ntplib.NTPClient()
     response = c.request('pool.ntp.org', version=3)
     return ctime(response.tx_time).encode('utf-8')
-    #tpm = 0
+    
+#tpm = 0
 def connect(ip):
-    con = bytearray(8)
-    con[0] = 0b0
+    con = bytearray(1)
+    con.append(0b0)
     array = ip.split(".")
     for a in range(len(array)):
         con.append(int(array[a]))
-    #con[5:8] = getTime()
+    con.append(getTime())
     print(con.decode)
+
     return con
 
 #tpm = 1
 def disconnect(ip):
     dis = bytearray(1)
-    dis[0] = 0b1
+    dis.append(0b1)
     array = ip.split(".")
 
     for a in range(len(array)):
         dis.append(int(array[a]))
-    dis.append(bytes(getTime()))
+    dis.append(getTime())
     return dis
 
 #tpm = 2
 def error(ip):
     err = bytearray(1)
-    err[0] = 0b10
+    err.append(0b10)
     array = ip.split(".")
 
     for a in range(len(array)):
         err.append(int(array[a]))
-    err.append(bytes(getTime()))
+    err.append(getTime())
     return err
 
     
 #tpm = 4
 def clock_adj(ip):
     c_adj = bytearray(1)
-    c_adj[0] = 0b100
+    c_adj.append(0b100)
     array = ip.split(".")
 
     for a in range(len(array)):
         c_adj.append(int(array[a]))
-    c_adj.append(bytes(getTime()))
+    c_adj.append(getTime())
     return c_adj
 
 def serverComm():
