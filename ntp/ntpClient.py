a0f8bf44a0bf406f1fde0126d5afc2a159ec5f1f
RuiFilipe2802 <ruifilipe1229@gmail.com>
2021-11-16 14:52:50 +0000

    added

--- ntp/ntpClient.py
+++ ntp/ntpClient.py
@@ -5,14 +5,26 @@ import sys
 import ntplib
 import socket
 import os
+import time
+import datetime
+from datetime import datetime
+import struct
 
-IP = "10.0.5.3"
+IP = "10.0.5.2"
 UDP_PORT = 6050
 
 if __name__ == "__main__":
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto("boas".encode('utf-8'), (IP, UDP_PORT))
-    print("boas")
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sock.sendto("boas".encode('utf-8'), (IP, UDP_PORT))
+    # print("boas")
 
-    data, addr = sock.recvfrom(1024)
-    print("received message: %s" % data.decode('utf-8'))
+    # data, addr = sock.recvfrom(1024)
+    # print("received message: %s" % data.decode('utf-8'))
+    start = time.time()
+    c = ntplib.NTPClient()
+    response = c.request('pool.ntp.org')
+    t = datetime.fromtimestamp(response.tx_time)
+    time_ntp = t.strftime("%d %m %H:%M:%S %Y")
+    print(time_ntp)
+    os.system('date ' +  t.strftime("%x"))
+    os.system('time ' +  t.strftime("%X"))
