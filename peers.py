d790f61e1f1e123741ef5280aa31b413579b49f8
julioliveira11 <a89393@alunos.uminho.pt>
2021-11-16 17:36:43 +0000

    Pouca

--- peers.py
+++ peers.py
@@ -4,9 +4,18 @@ from time import *
 import _thread
 import ntplib
 from datetime import datetime
+import sys
 host = '192.168.58.248'  # Standard loopback interface address (localhost)
 port = 9998      # Port to listen on (non-privileged ports are > 1023)
 port2 = 5000
+
+ip_source=""
+ip_dest1=""
+ip_dest2=""
+            
+porta_source=0
+porta_dest1=0
+porta_dest2=0
 
 def getTime():
     c = ntplib.NTPClient()
@@ -47,6 +56,23 @@ def error(ip):
     err.append(getTime())
     return err
 
+def package_interpretation(Response):
+    if Response[0] == 3:
+        for a in range(len(Response[1:4])):
+            ip_source=a.join(".")
+
+        for b in range(len(Response[5:8])):
+            ip_dest1=b.join(".")
+
+        for c in range(len(Response[9:12])):
+            ip_dest2=c.join(".")
+
+        porta_source=Response[13:14]
+        porta_dest1=Response[15:16]
+        porta_dest2=Response[17:18]
+
+    
+
     
 #tpm = 4
 def clock_adj(ip):
@@ -79,21 +105,80 @@ def serverComm():
         Response = ClientSocket.recv(1024)
         print(Response.decode('utf-8'))
 
+        if Response[0] == 3:
+            
+
+            for a in range(Response[1:4]):
+                ip_source.join(a.join("."))
+
+            for b in range(Response[5:8]):
+                ip_dest1.join(b.join("."))
+
+            for c in range(Response[9:12]):
+                ip_dest2.join(c.join("."))
+
+
+            for d in range(Response[13:14]):
+                porta_source.join(d)
+
+            for e in range(Response[15:16]):
+                porta_dest1.join(e)
+
+            for f in range(Response[17:18]):
+                porta_dest2.join(f)
+            
+
+
     ClientSocket.close()
 
 def peerServer():
     
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(("", port))
+    s.bind(("127.0.0.1", port2))
     print ("waiting on port:", port2)
     while 1:
         data, addr = s.recvfrom(1024)
         print(data)
 
+def peerClient(ip_dest,porta):
+    # create dgram udp socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error:
+        print ('Failed to create socket')
+        sys.exit()
+
+
+    while(1) :
+        msg = bytes('Pouca treta'.encode())
+        
+        try :
+            #Set the whole string
+            s.sendto(msg, (ip_dest, porta))
+            
+            # receive data from client (data, addr)
+            d = s.recvfrom(1024)
+            reply = d[0]
+            addr = d[1]
+            
+            print ('Server reply : ' + reply)
+        
+        except socket.error :
+            print ('Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+            sys.exit()  
+
+
+
 if __name__ == "__main__":
-    print(getTime())
+
     
     _thread.start_new_thread(serverComm,())
+    
+
+    _thread.start_new_thread(peerServer,())
+
+    _thread.start_new_thread(peerClient,(ip_dest1,porta_dest1))
+    _thread.start_new_thread(peerClient,(ip_dest2,porta_dest2))
 
 
     while 1:
