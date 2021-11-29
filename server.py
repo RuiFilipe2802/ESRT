90c77d9ae88b20b53c896ab38cdbb9b467cbe07b
JoaoFerreira5 <joao.ferreira.585@gmail.com>
2021-11-29 16:56:09 +0000

    server

--- server.py
+++ server.py
@@ -18,18 +18,18 @@ verificar_mensagens = []
 lock = threading.Lock()
 N_thread = 0
 threadCount = 0
+peers_connected = 0
 
 #pass joao = Johnny1999@
 
 mydb = mysql.connector.connect(
   host = "localhost",
   user = "root",
-  password = "Johnny1999@",
+  password = "johnny1999",
   database = "server_database"
 )
 
 mycursor = mydb.cursor()
-
 
 
 def getTime():
@@ -38,33 +38,20 @@ def getTime():
     return response.tx_time
 
 #tpm = 3  
-def send_neighbors(porta,n_peers,n_vizinhos,ip1,porta1,ip2,porta2):
+def send_neighbors(porta, n_vizinhos, ip1, ip2):
     send = bytearray(1)
     send[0] = 3
     #send.append(ip1.encode())
     array2 = ip1.split(".")
     array3 = ip2.split(".")
-    b_p = porta.to_bytes(2,'big')
-    for i in range(len(b_p)):
-        send.append(i)
-
-    send.append(n_peers)
+    
     send.append(n_vizinhos)
 
     for b in range(len(array2)):
         send.append(int(array2[b]))
     
-    b_p1 = porta1.to_bytes(2,'big')
-    for i in range(len(b_p1)):
-        send.append(i)
-    
     for c in range(len(array3)):
         send.append(int(array3[c]))
-
-    b_p2 = porta2.to_bytes(2,'big')
-    
-    for i in range(len(b_p2)):
-        send.append(i)
 
     print(send)
     return send
@@ -94,38 +81,74 @@ def add_peer_database(id,ip,porta,status):
     mycursor.execute("INSERT INTO peer(id,ip,porta,status) VALUES (%s,%s,%s,%s)",(id,ip,porta,status))
     mydb.commit()
 
+def set_status_on(id):
+    sql = "UPDATE peer set status = 1 WHERE id = "+str(id)
+    mycursor.execute(sql)
+    mydb.commit()
+
+def verificar_status(b):
+    print("entrei2")
+    sql = "SELECT status FROM peer WHERE id = "+str(b)+""
+    mycursor.execute(sql)
+    aux = mycursor.fetchone()
+    print(aux)
+    aux1 = str(aux[0])
+    status = int(aux1.strip("[").strip("(").strip("'").strip(")").strip("]").strip(",").strip("'"))
+    print("status " +str(status))
+    if status == 0:
+        return False
+    else: 
+        return True
+
+def qnt_peers_on():
+    soma = 0
+    print("entrei")
+    for a in range(threadCount):
+        if verificar_status(a):
+            soma = soma + 1
+    print("soma de peers"+str(soma))
+    return soma
+
+def get_ip_neighbor(id1):
+    sql = "SELECT ip FROM peer WHERE id = "+id1+""
+    mycursor.execute(sql)
+    aux = mycursor.fetchall()
+    aux1 = str(aux[0])
+    ip1 = aux1.strip("[").strip("(").strip("'").strip(")").strip("]").strip(",").strip("'")
+    return ip1
+    
+
 def atribuir_vizinhos(id):
     numero_ids = []
     print(threadCount)
-    if threadCount == 2:
-        if id == 0:
-            print("vou adicionar 1")
-            numero_ids.append(1)
-            print(numero_ids)
-            return numero_ids
-        else:
-            print("vou adicionar 0")
-            numero_ids.append(0)
-            print(numero_ids)
-            return numero_ids
-    else:
-        for a in range(N):
-            b = random.randint(0,threadCount)
-            if b != id and b not in numero_ids:
+    if qnt_peers_on() == 2:
+        a = 0
+        while a != 1:
+            a += 1
+            b = random.randint(0,threadCount - 1 )
+            if b != id and b not in numero_ids and verificar_status(b):
                 numero_ids.append(b)
-            else: 
+            else:
+                a = a - 1
+        return numero_ids
+    else:
+        print(str(id) + " <-id; threadcount->" + str(threadCount))
+        a = 0
+        while a != 2:
+            a += 1
+            b = random.randint(0,threadCount - 1 )
+            if b != id and b not in numero_ids and verificar_status(b):
+                numero_ids.append(b)
+            else:
                 a = a - 1
         return numero_ids
 
 def thread_listening(connect, n_t):
     while True:
         data = connect.recv(2048)
-        if threadCount == 1 and data[0] == 0:
-            connect.send(b'-1')
-        else:
-            verificar_mensagens[n_t] = 1
-            lista_mensagens[n_t] = data
-            print(str(lista_mensagens[n_t]) + " numero: "+ str(n_t))
+        verificar_mensagens[n_t] = 1
+        lista_mensagens[n_t] = data
+        print(str(lista_mensagens[n_t]) + " numero: "+ str(n_t))
 
 
 def thread_client(connection,n_thread, listening_port):
@@ -133,64 +156,43 @@ def thread_client(connection,n_thread, listening_port):
     start_new_thread(thread_listening,(connection, n_thread,))
     while True:
         #interpretar data de modo a ver o que o peer quer fazer, ou conectar ou desconectar
-        #lock.acquire()
+        lock.acquire()
         if verificar_mensagens[n_thread] == 1:
             data = lista_mensagens[n_thread]
             if data[0] == 0:
+                aux = True
+                set_status_on(n_thread)
+                #peers_connected = peers_connected + 1
+                if qnt_peers_on() == 1:
+                    connection.send(b'-1')
+                    aux = False
                 print("tpm 0")
-                viz_ids = []
-                viz_ids = atribuir_vizinhos(n_thread)
-                print("vizinhos: "+ str(viz_ids))
-                if viz_ids is None:
-                    print("Esperar por mais conexões para iniciar rede overlay vizinhos")
-                else: 
-                    if threadCount == 2:
-                        #enviar apenas um vizinho, o 2 ip vai com 0.0.0.0 e porta a 0
-                        sql = "SELECT ip FROM peer WHERE id = "+str(viz_ids[0])+""
-                        mycursor.execute(sql)
-                        aux = mycursor.fetchall()
-                        aux1 = str(aux[0])
-                        ip1 = aux1.strip("[").strip("(").strip("'").strip(")").strip("]").strip(",").strip("'")
-                        print(ip1)
-                        sql = "SELECT porta FROM peer WHERE id = "+str(viz_ids[0])+""
-                        mycursor.execute(sql)
-                        aux = mycursor.fetchall()
-                        aux1 = str(aux[0])
-                        porta = int(aux1.strip("[").strip("(").strip("'").strip(")").strip("]").strip(","))
-                        print(porta)
-                        packet = send_neighbors(listening_port,threadCount,1, ip1, porta, '0', 0)
-                    else:
-                        sql = "SELECT ip FROM peer WHERE id = "+str(viz_ids[0])+""
-                        mycursor.execute(sql)
-                        aux = mycursor.fetchall()
-                        aux1 = str(aux[0])
-                        ip1 = aux1.strip("[").strip("(").strip("'").strip(")").strip("]").strip(",").strip("'")
-                        print(ip1)
-                        sql = "SELECT ip FROM peer WHERE id = "+str(viz_ids[0])+""
-                        mycursor.execute(sql)
-                        aux = mycursor.fetchall()
-                        aux1 = str(aux[0])
-                        ip2 = aux1.strip("[").strip("(").strip("'").strip(")").strip("]").strip(",").strip("'")
-                        sql = "SELECT porta FROM peer WHERE id = "+str(viz_ids[0])+""
-                        mycursor.execute(sql)
-                        aux = mycursor.fetchall()
-                        aux1 = str(aux[0])
-                        porta1 = int(aux1.strip("[").strip("(").strip(")").strip("]").strip(","))
-                        sql = "SELECT porta FROM peer WHERE id = "+str(viz_ids[0])+""
-                        mycursor.execute(sql)
-                        aux = mycursor.fetchall()
-                        aux1 = str(aux[0])
-                        porta2 = int(aux1.strip("[").strip("(").strip(")").strip("]").strip(","))
-                        packet = send_neighbors(listening_port,threadCount,N, ip1, porta1,ip2,porta2)
+                if aux:
+                    viz_ids = []
+                    viz_ids = atribuir_vizinhos(n_thread)
+                    print("vizinhos: "+ str(viz_ids))
+                    if viz_ids is None:
+                        print("Esperar por mais conexões para iniciar rede overlay vizinhos")
+                    else: 
+                        if threadCount == 2:
+                            #enviar apenas um vizinho, o 2 ip vai com 0.0.0.0 e porta a 0
+                            ip1 = get_ip_neighbor(viz_ids[0])
+                            packet = send_neighbors(listening_port,1, ip1, '0')
+                        else:
+                            ip1 = get_ip_neighbor(viz_ids[0])
+                            ip2 = get_ip_neighbor(viz_ids[1])
+                            packet = send_neighbors(listening_port,N, ip1,ip2)
 
                 #connection.send(send_neighbors("120.20","121.1","122.2",5,4,3))
-                connection.send(packet)
+                    connection.send(packet)
             elif data[0] == 1:
                 print("tpm 1")
                 connection.send("ole".encode('utf-8'))
+            elif data[0] == 10:
+                print("recebi um pacote de encaminhamaneto")
             verificar_mensagens[n_thread] = 0
             print("passei aqui")
-        #lock.release
+        lock.release()
          
         if variavel_broadcast == 1:
             #avisar que conectou
@@ -204,9 +206,7 @@ if __name__ == "__main__":
     portas_peer = 5000
     
     ServerSocket = socket.socket()
-    #threadCount = 0
-    #ServerSocket.setblocking(0)
-    #ServerSocket.settimeout(4)
+    
     try:
         ServerSocket.bind((host,port))
         
